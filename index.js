efed8c0452d3b8e160c53b4a4d46502be8ed129e
perugi <dominik.perusko@gmail.com>
2024-01-09 19:49:00 +0100

    Remove unneeded configuration (to be part of the project config

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+export const ESLintConfig = {
   rules: {
     "no-console": "off",
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
   },
-  // Make sure this is last, so it gets the chance to override the other configurations.
-  extends: ["airbnb-base", "prettier"],
 };
