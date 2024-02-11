eb48f3c3dee7f006da956382d175060b0eface8e
perugi <dominik.perusko@gmail.com>
2024-02-11 22:25:12 +0100

    Allow underscores in class fields and methods

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ module.exports = {
     "no-param-reassign": ["error", { props: false }],
     "import/extensions": ["error", "never", { mjs: "always" }],
     "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "no-underscore-dangle": [
+      "error",
+      {
+        enforceInMethodNames: false,
+        enforceInClassFields: false,
+        allowAfterThis: true,
+        allowAfterSuper: true,
+      },
+    ],
   },
 };
